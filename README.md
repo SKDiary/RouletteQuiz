5918f122d1604ce6ab1c22e724acd04ad1ee246c
SKDiary <Knitta1127@gmail.com>
2023-10-28 19:02:12 +0900

    28 october-3

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+package.jsonに
+"homepage": "https://SKDiary.github.io/RouletteQuiz",
+を追加
--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import QuizMain from "./pages/QuizMain.jsx";
+import Result from "./pages/Result";
+import { useState } from "react";
+import QuizHome from "./pages/QuizHome";
+import Library from "./pages/Library";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [problemList, setProblemList] = useState([]);
+	const [answerList, setAnswerList] = useState([]);
+	const [difficulty, setDifficulty] = useState(0);
+
+	const ROUTER_BASENAME = process.env.NODE_ENV === "development" ? "/" : "/RouletteQuiz";
+
+	return (
+		<Router basename={ROUTER_BASENAME}>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<QuizHome
+							difficulty={difficulty}
+							setDifficulty={setDifficulty}
+							setProblemList={setProblemList}
+						/>
+					}
+				/>
+				<Route
+					path="/quiz"
+					element={
+						<QuizMain
+							problemList={problemList}
+							answerList={answerList}
+							setAnswerList={setAnswerList}
+							difficulty={difficulty}
+						/>
+					}
+				/>
+				<Route
+					path="/result"
+					element={<Result problemList={problemList} answerList={answerList} difficulty={difficulty} />}
+				/>
+				<Route path="/library" element={<Library />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
